7d9110444f902d971c81e89e9aeaadf228c4ddcf
Dmitri Osipov <dmitri.osipov@lexistems.com>
2021-05-02 01:04:28 +0300

    LED fine-tuning

--- conditional_sum-simple-mBART-extended.py
+++ conditional_sum-simple-mBART-extended.py
@@ -66,29 +66,29 @@ for i in src_list:
 if __name__ == '__main__':
     with open(args.output_file, 'w') as f:
         start_time = time.time()
-        for input_string in src_list_clean[0:5]:
+        for input_string in src_list_clean[0:100]:
 
             # summary = generate_answer(batch=input_string, device=device)
 
-            # input_ids = tokenizer(input_string, return_tensors="pt", max_length=args.input_max_length,
-            #                       padding=True, truncation=True).input_ids.to(device)
+            input_ids = tokenizer([input_string], return_tensors="pt", padding="max_length",
+                                  max_length=args.input_max_length, truncation=True).input_ids.to(device)
             # global_attention_mask = torch.zeros_like(input_ids)
             # # set global_attention_mask on first token
             # global_attention_mask[:, 0] = 1
-            # sequences = model.generate(input_ids, global_attention_mask=global_attention_mask, max_length=args.sum_max_length,
-            #                            length_penalty=args.length_penalty, num_beams=args.num_beams, early_stopping=True).sequences
-            # summary = tokenizer.batch_decode(sequences)
+            sequences = model.generate(input_ids, max_length=args.sum_max_length,
+                                       length_penalty=args.length_penalty, num_beams=args.num_beams, early_stopping=True).sequences
+            summary = tokenizer.batch_decode(sequences)
 
-            inputs = tokenizer.encode(input_string, return_tensors='pt', padding="max_length",
-                                      max_length=args.input_max_length, truncation=True).to(device)
-            summary_ids = model.generate(inputs, max_length=args.sum_max_length, length_penalty=args.length_penalty,
-                                    num_beams=args.num_beams, early_stopping=True)
-
-            for g in summary_ids:
-                summary = tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False)
-                print(summary)
-                f.write(summary + "\n")
-                f.flush()
+            # inputs = tokenizer.encode([input_string], return_tensors='pt', padding="max_length",
+            #                           max_length=args.input_max_length, truncation=True).to(device)
+            # summary_ids = model.generate(inputs['input_ids'], max_length=args.sum_max_length, length_penalty=args.length_penalty,
+            #                         num_beams=args.num_beams, early_stopping=True)
+            #
+            # for g in summary_ids:
+            #     summary = tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False)
+            print(summary)
+            f.write(summary + "\n")
+            f.flush()
         f.close()
         runtime = int(time.time() - start_time)
         n_obs = len(src_list_clean)
