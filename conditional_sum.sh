c4ec4075c973deac0e63bd726712e70daa81e8cf
Dmitri Osipov <dmitri.osipov@lexistems.com>
2021-05-01 18:00:01 +0300

    correct path

--- conditional_sum.sh
+++ conditional_sum.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 python conditional_sum-mBART-extended.py \
-	--model_directory LongMBART-25-8K-wiki-es-15epochs \
+	--model_directory ../LongMBART-25-8K-wiki-es-15epochs \
 	--input_file /data/summarization/wikipedia_data/clean_es/clean_es/test.source \
 	--output_file ./evaluation/LongmBART-25-8K-wiki-15-extended.txt \
 	--eval_file ./evaluation/metrics-LongmBART-25-8K-wiki-15-extended.json \
