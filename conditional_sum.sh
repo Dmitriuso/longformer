78a2d5779143927800ba8079e4e8f7748242fc50
Dmitri Osipov <dmitri.osipov@lexistems.com>
2021-05-01 23:11:30 +0300

    test files altered

--- conditional_sum.sh
+++ conditional_sum.sh
@@ -4,7 +4,7 @@ python conditional_sum-simple-mBART-extended.py \
 	--input_file /data/summarization/wikipedia_data/clean_es/test.source \
 	--output_file ./evaluation/LongmBART-25-8K-wiki-15-extended.txt \
 	--eval_file ./evaluation/metrics-LongmBART-25-8K-wiki-15-extended.json \
-	--input_max_length 8192 \
+	--input_max_length 512 \
 	--sum_max_length 128 \
 	--num_beams 2 \
   	--length_penalty 0.7 \
